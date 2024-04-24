8c020b6c05e86afa63e316f2750b9face1b07813
denzel84 <baderou@yahoo.fr>
2024-04-24 14:43:39 +0100

    taskdone

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-# start a flask web app
+"""start a flask web app"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -11,13 +11,13 @@ port = 5000
 
 @app.route('/')
 def hello_hbnb():
-    # says Hello HBNB when curl
+    """says Hello HBNB when curl"""
     return("Hello HBNB!")
 
 
 @app.route('/hbnb')
 def hbnb():
-    # displays HBNB
+    """displays HBNB"""
     return ("HBNB")
 
 
