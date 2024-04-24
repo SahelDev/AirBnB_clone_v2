7a6eac9403d39921ad0bdf12db6450e5ec277e7d
denzel84 <baderou@yahoo.fr>
2024-04-24 15:04:59 +0100

    task done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -17,4 +17,3 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     app.run(host=ip, port=port)
-    
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-#start a flask web app
+'''start a flask web app'''
 from flask import Flask
-app= Flask(__name__)
+app = Flask(__name__)
 app.url_map.strict_slashes = False
-ip='0.0.0.0'
+ip = '0.0.0.0'
 port = 5000
+
 
 @app.route('/')
 def hello_hbnb():
-    #says Hello HBNB when curl'd
+    '''says Hello HBNB when curl'd'''
     return("Hello HBNB")
+
 
 @app.route('/hbnb')
 def hbnb():
-    # displays HBNB
+    '''displays HBNB'''
     return ("HBNB")
+
 
 @app.route('/c/<text>')
 def c_text(text):
-    # returns user text with C in front
+    '''returns user text with C in front'''
     return ("C {}".format(text.replace("_", " ")))
+
 
 if __name__ == "__main__":
     app.run(host=ip, port=port)
-    
-
-
