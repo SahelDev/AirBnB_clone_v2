a8dadd41e3f4d5062f60cf4cc7aaaa12fbe805ad
denzel84 <baderou@yahoo.fr>
2024-04-24 15:10:45 +0100

    task 04

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-# start a flask web app
+"""start a flask web app"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -11,26 +11,26 @@ port = 5000
 
 @app.route('/')
 def hello_hbnb():
-    # says Hello HBNB when curl'd
+    """says Hello HBNB when curl'd"""
     return("Hello HBNB")
 
 
 @app.route('/hbnb')
 def hbnb():
-    # displays HBNB
+    """displays HBNB"""
     return ("HBNB")
 
 
 @app.route('/c/<text>')
 def c_text(text):
-    # returns user text with C in front
+    """returns user text with C in front"""
     return ("C {}".format(text.replace("_", " ")))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
-    # returns user text with python in front
+    """returns user text with python in front"""
     return ("Python {}".format(text.replace("_", " ")))
 
 
